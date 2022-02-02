52bce55fb82ccd25aa11c856027fb286629687f6
Alex Pissot <ylex@hotmail.fr>
2022-02-02 12:42:18 +0100

    Ajout pagination

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,10 +6,12 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 /**
  * @Route("/category")
@@ -19,10 +21,17 @@ class CategoryController extends AbstractController
     /**
      * @Route("/", name="category_index", methods={"GET"})
      */
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $categoryQuery = $categoryRepository->findall();
+
+        $categories = $paginator->paginate(
+            $categoryQuery, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            2 /*limit per page*/
+        );
         return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
+            'categories' => $categories
         ]);
     }
 
